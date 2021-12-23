3e117b4baeffeaeb7ac4243a14e092930ff1761a
FabijanC <fabijan.corak@gmail.com>
2021-12-23 11:45:45 +0100

    Extend sleep and add docker logs in image script

--- scripts/image_build_and_push.sh
+++ scripts/image_build_and_push.sh
@@ -12,8 +12,9 @@ docker build -t "$IMAGE:$LOCAL_VERSION" -t "$IMAGE:latest" .
 
 echo "Run a devnet instance in background; sleep to allow it to start"
 # can't use "localhost" because docker doesn't allow such mapping
-docker run -d -p 127.0.0.1:5000:5000 "$IMAGE:latest"
-sleep 3
+docker run -d -p 127.0.0.1:5000:5000 --name devnet "$IMAGE:latest"
+sleep 10
+docker logs devnet
 
 echo "Checking if devnet instance is alive"
 ssh remote-docker curl localhost:5000/is_alive
