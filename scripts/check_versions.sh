851b2e8e8461f3c2986abaa43854559cc83e34e4
FabijanC <fabijan.corak@gmail.com>
2022-04-20 11:11:01 +0200

    Add versioning consistency check (#87) [skip ci]

---
+++ scripts/check_versions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+TOML_VERSION=$(./scripts/get_version.sh version)
+echo "toml version: $TOML_VERSION"
+PY_VERSION=$(python -c "from starknet_devnet import __version__ as v; print(v)")
+echo "py version: $PY_VERSION"
+
+if [ "$TOML_VERSION" != "$PY_VERSION" ]; then
+    echo "Inconsistent versioning across project"
+    exit 1
+fi
