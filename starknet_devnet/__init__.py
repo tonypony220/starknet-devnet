6f3a7fc26eeb00ccdf471b885c9c3556fc5963da
Antun Badurina <31538513+badurinantun@users.noreply.github.com>
2022-04-20 11:31:56 +0200

    Test tx version (#86)
    * Add tx_version test
    
    * Bump version to 0.1.22

--- starknet_devnet/__init__.py
+++ starknet_devnet/__init__.py
@@ -2,4 +2,4 @@
 Contains the server implementation and its utility classes and functions.
 """
 
-__version__ = "0.1.21"
+__version__ = "0.1.22"
---
+++ test/test_tx_version.py
@@ -0,0 +1,29 @@
+"""
+Test transaction version
+"""
+
+import pytest
+
+from starkware.starknet.definitions.constants import TRANSACTION_VERSION, QUERY_VERSION
+
+from .util import devnet_in_background, deploy, call, invoke
+from .shared import ARTIFACTS_PATH
+
+CONTRACT_PATH = f"{ARTIFACTS_PATH}/tx_version.cairo/tx_version.json"
+ABI_PATH = f"{ARTIFACTS_PATH}/tx_version.cairo/tx_version_abi.json"
+
+@pytest.mark.tx_version
+@devnet_in_background()
+def test_transaction_version():
+    """Check if transaction versions are correct"""
+    deploy_info = deploy(CONTRACT_PATH)
+    address = deploy_info["address"]
+
+    invoke("set_tx_version", [], address, ABI_PATH)
+
+    invoke_tx_version = call("get_last_tx_version", address, ABI_PATH)
+    assert int(invoke_tx_version, 16) == TRANSACTION_VERSION
+
+    call_tx_version = call("get_tx_version", address, ABI_PATH)
+
+    assert int(call_tx_version, 16) == QUERY_VERSION
