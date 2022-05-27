e6dc8688ed59d134fa959c75b68d7deda94a77ba
FabijanC <fabijan.corak@gmail.com>
2022-05-27 13:16:33 +0200

    Fix check_versions script

--- scripts/check_versions.sh
+++ scripts/check_versions.sh
@@ -4,7 +4,7 @@ set -e
 
 TOML_VERSION=$(./scripts/get_version.sh version)
 echo "toml version: $TOML_VERSION"
-PY_VERSION=$(python -c "from starknet_devnet import __version__ as v; print(v)")
+PY_VERSION=$(poetry run python -c "from starknet_devnet import __version__ as v; print(v)")
 echo "py version: $PY_VERSION"
 
 if [ "$TOML_VERSION" != "$PY_VERSION" ]; then
