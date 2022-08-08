812a98a53dfb0bb6ee35b9510900ded5b4588806
FabijanC <fabijan.corak@gmail.com>
2022-08-08 17:27:25 +0200

    Fix block number off-by-one (#214)
    * [skip ci] because a commit which releases a new version follows

--- test/test_block_number.py
+++ test/test_block_number.py
@@ -2,8 +2,8 @@
 Test block number
 """
 
-from .shared import ARTIFACTS_PATH, GENESIS_BLOCK_NUMBER
-from .util import create_empty_block, devnet_in_background, deploy, call
+from .shared import ARTIFACTS_PATH, FAILING_CONTRACT_PATH, GENESIS_BLOCK_NUMBER
+from .util import declare, devnet_in_background, deploy, call, invoke
 
 BLOCK_NUMBER_CONTRACT_PATH = f"{ARTIFACTS_PATH}/block_number.cairo/block_number.json"
 BLOCK_NUMBER_ABI_PATH = f"{ARTIFACTS_PATH}/block_number.cairo/block_number_abi.json"
@@ -22,18 +22,81 @@ def base_workflow():
     block_number_before = my_get_block_number(deploy_info["address"])
     assert int(block_number_before) == GENESIS_BLOCK_NUMBER + 1
 
-    # generate a new block
-    create_empty_block()
+    invoke(
+        function="write_block_number",
+        inputs=[],
+        address=deploy_info["address"],
+        abi_path=BLOCK_NUMBER_ABI_PATH
+    )
+
+    written_block_number = call(
+        function="read_block_number",
+        inputs=[],
+        address=deploy_info["address"],
+        abi_path=BLOCK_NUMBER_ABI_PATH
+    )
+    assert int(written_block_number) == GENESIS_BLOCK_NUMBER + 2
 
     block_number_after = my_get_block_number(deploy_info["address"])
     assert int(block_number_after) == GENESIS_BLOCK_NUMBER + 2
 
 @devnet_in_background()
 def test_block_number_incremented():
-    """Tests how block number is incremented with"""
+    """Tests how block number is incremented in regular mode"""
     base_workflow()
 
 @devnet_in_background("--lite-mode")
 def test_block_number__incremented_in_lite_mode():
     """Tests compatibility with lite mode"""
     base_workflow()
+
+@devnet_in_background()
+def test_block_number_incremented_on_declare():
+    """Declare tx should increment get_block_number response"""
+
+    deploy_info = deploy(BLOCK_NUMBER_CONTRACT_PATH)
+    block_number_before = my_get_block_number(deploy_info["address"])
+    assert int(block_number_before) == GENESIS_BLOCK_NUMBER + 1
+
+    # just to declare a new class - nothing fails here
+    declare(FAILING_CONTRACT_PATH)
+
+    block_number_after = my_get_block_number(deploy_info["address"])
+    assert int(block_number_after) == GENESIS_BLOCK_NUMBER + 2
+
+@devnet_in_background()
+def test_block_number_not_incremented_if_deploy_fails():
+    """
+    Since the deploy fails, no block should be created;
+    get_block_number should return an unchanged value
+    """
+
+    deploy_info = deploy(BLOCK_NUMBER_CONTRACT_PATH)
+    block_number_before = my_get_block_number(deploy_info["address"])
+    assert int(block_number_before) == GENESIS_BLOCK_NUMBER + 1
+
+    deploy(FAILING_CONTRACT_PATH)
+
+    block_number_after = my_get_block_number(deploy_info["address"])
+    assert int(block_number_after) == GENESIS_BLOCK_NUMBER + 1
+
+@devnet_in_background()
+def test_block_number_not_incremented_if_invoke_fails():
+    """
+    Since the invoke fails, no block should be created;
+    get_block_number should return an unchanged value
+    """
+
+    deploy_info = deploy(BLOCK_NUMBER_CONTRACT_PATH)
+    block_number_before = my_get_block_number(deploy_info["address"])
+    assert int(block_number_before) == GENESIS_BLOCK_NUMBER + 1
+
+    invoke(
+        function="fail",
+        inputs=[],
+        address=deploy_info["address"],
+        abi_path=BLOCK_NUMBER_ABI_PATH,
+    )
+
+    block_number_after = my_get_block_number(deploy_info["address"])
+    assert int(block_number_after) == GENESIS_BLOCK_NUMBER + 1
