5fcf710d037e9fc38de8476eff7281fb6b9e084a
Jakub Ptak <jakub.ptak@swmansion.com>
2022-10-06 10:17:17 +0200

    Faster contract deploy (#299)

--- starknet_devnet/__init__.py
+++ starknet_devnet/__init__.py
@@ -3,13 +3,13 @@ Contains the server implementation and its utility classes and functions.
 """
 import sys
 from copy import copy
+import starkware.cairo.lang.vm.crypto
 from starkware.crypto.signature.fast_pedersen_hash import pedersen_hash
 from starkware.starknet.services.api.contract_class import ContractClass
 from crypto_cpp_py.cpp_bindings import cpp_hash
 
 
 __version__ = "0.3.2"
-
 
 
 def patched_pedersen_hash(left: int, right: int) -> int:
@@ -24,6 +24,11 @@ def patched_pedersen_hash(left: int, right: int) -> int:
 # instead of python implementation from cairo-lang package
 setattr(
     sys.modules["starkware.crypto.signature.fast_pedersen_hash"],
+    "pedersen_hash",
+    patched_pedersen_hash,
+)
+setattr(
+    sys.modules["starkware.cairo.lang.vm.crypto"],
     "pedersen_hash",
     patched_pedersen_hash,
 )
