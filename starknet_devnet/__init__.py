3a22f0c974b828409c471d0020d0da21a393fae4
FabijanC <fabijan.corak@gmail.com>
2023-03-23 19:27:48 +0100

    Bump version to v0.5.0a0

--- starknet_devnet/__init__.py
+++ starknet_devnet/__init__.py
@@ -13,7 +13,7 @@ This file contains monkeypatches used across the project. Advice for monkeypatch
 import os
 import sys
 
-__version__ = "0.4.6"
+__version__ = "0.5.0a0"
 
 
 def _patch_pedersen_hash():
