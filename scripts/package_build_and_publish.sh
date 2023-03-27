a7f99f1db13fd19dad8a5cc813458180f5f06887
FabijanC <fabijan.corak@gmail.com>
2023-03-27 17:14:48 +0200

    Fix publishing script [skip ci]

--- scripts/package_build_and_publish.sh
+++ scripts/package_build_and_publish.sh
@@ -3,30 +3,35 @@ set -eu
 
 [ -f .env ] && source .env
 
-PYPI_VERSION=$(curl -Ls https://pypi.org/pypi/starknet-devnet/json | jq -r .info.version)
-echo "Pypi version: $PYPI_VERSION"
+PYPI_VERSIONS=$(curl -Ls https://pypi.org/pypi/starknet-devnet/json | jq '.releases|keys')
+echo "PyPI versions: $PYPI_VERSIONS"
 
 LOCAL_VERSION=$(./scripts/get_version.sh version)
 echo "Local version: $LOCAL_VERSION"
 
+# locate the local version inside the keys array; "null" if not present
+LOCAL_VERSION_INDEX=$(echo "$PYPI_VERSIONS" | jq "index( \"$LOCAL_VERSION\" )")
+
 # Building is executed regardles of versions
 poetry build
 
-if [ "$PYPI_VERSION" = "$LOCAL_VERSION" ]; then
-    echo "Latest pypi version is already equal to the local version."
+if [ "$LOCAL_VERSION_INDEX" != "null"  ]; then
+    echo "Latest PyPI version is already equal to the local version."
     echo "Publishing skipped"
 else
+    echo "would publish"
+    exit 0
+    # publish Python package
     poetry publish --username "$PYPI_USER" --password "$PYPI_PASS"
 
     # publish docs
-
     cd page && npm ci
 
     git config --global user.email "$GIT_USER@users.noreply.github.com"
     git config --global user.name "$GIT_USER"
     echo "machine github.com login $GIT_USER password $GITHUB_TOKEN" > ~/.netrc
 
-    # skip ci to avoid gh-pages erroring on circleci
+    # [skip ci] to avoid gh-pages erroring on circleci
     LATEST_COMMIT_HASH=$(git rev-parse HEAD)
     CUSTOM_COMMIT_MESSAGE="Deploy website - based on $LATEST_COMMIT_HASH [skip ci]" \
         npm run deploy
